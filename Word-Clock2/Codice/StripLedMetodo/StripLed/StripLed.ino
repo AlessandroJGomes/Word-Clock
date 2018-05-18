6a36b1a074c015f71ed972924440d4b247acc1c4
AlessandroGomes <alessandro.gomes@samtrevano.ch>
2018-05-18 19:09:09 +0200

    Diario ultima lezione

--- Word-Clock2/Codice/StripLedMetodo/StripLed/StripLed.ino
+++ Word-Clock2/Codice/StripLedMetodo/StripLed/StripLed.ino
@@ -9,47 +9,74 @@ bool dir = true;
 //   NEO_GRB     Pixels are wired for GRB bitstream, correct for neopixel stick
 //   NEO_KHZ400  400 KHz bitstream (e.g. FLORA pixels)
 //   NEO_KHZ800  800 KHz bitstream (e.g. High Density LED strip), correct for neopixel stick
-Adafruit_NeoPixel strip = Adafruit_NeoPixel(144, PIN, NEO_GRB + NEO_KHZ800);
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(195, PIN, NEO_GRB + NEO_KHZ800);
+int sonoLeTre[9] = {28, 51, 54, 77, 103, 106, 23, 30, 49};
 
 void setup() {
     strip.begin();
-    strip.setBrightness(50);
+    strip.setBrightness(500);
     strip.show();
 }
 
 void loop() {
-  /*
-    if(i == 143){
+
+    
+    strip.setPixelColor(i-6, 0, 0, 0);
+    strip.setPixelColor(i-5, 0, 0, 0);
+    strip.setPixelColor(i-4, 0, 0, 0);
+    strip.setPixelColor(i-3, 0, 0, 0);
+    strip.setPixelColor(i-2, 0, 0, 0);
+    strip.setPixelColor(i-1, 0, 0, 0);
+    strip.setPixelColor(i, 0, 0, 0);
+    strip.setPixelColor(i+1, 0, 0, 0);
+    strip.setPixelColor(i+2, 0, 0, 0);
+    strip.setPixelColor(i+3, 0, 0, 0);
+    strip.setPixelColor(i+4, 0, 0, 0);
+    strip.setPixelColor(i+5, 0, 0, 0);
+    strip.setPixelColor(i+6, 0, 0, 0);
+    
+    if(i == 195){
       dir = false;
     }
     else if(i == 0){
       dir = true;
     }
-    
     if(dir){
       i++;
     }
     else{
       i--;
     }
-    //strip.setPixelColor(i, 255, 0, 255);
-    if(i%2 == 0){
-      strip.setPixelColor(i, 255, 0, 0);
-    }
-    else if(i%3 == 0){
-      strip.setPixelColor(i, 0, 255, 0);    
-    }
-    else{
-      strip.setPixelColor(i, 0, 0, 255); 
-    }
-    strip.show();
+    /**
+    int riga = i % 13;
+    int colonna = ceil(i / 13);
+    if(i % 2 != 0){
+      riga = 13 - riga;
+    }*/
+    
+    
+    
+    
+    strip.setPixelColor(i-6, 255, 0, 0);
+    strip.setPixelColor(i-5, 0, 255, 0);
+    strip.setPixelColor(i-4, 0, 0, 0);
+    strip.setPixelColor(i-3, 255, 0, 0);
+    strip.setPixelColor(i-2, 0, 255, 0);
+    strip.setPixelColor(i-1, 0, 0, 0);
     strip.setPixelColor(i, 0, 0, 0);
-    */
-
-    for(int j = 0; j < 144; j++){
-
-        strip.setPixelColor(j, 255, 0, 0);
-    }
+    strip.setPixelColor(i+1, 255, 0, 0);
+    strip.setPixelColor(i+2, 0, 255, 0);
+    strip.setPixelColor(i+3, 0, 0, 0);
+    strip.setPixelColor(i+4, 255, 0, 0);
+    strip.setPixelColor(i+5, 0, 255, 0);
+    strip.setPixelColor(i+6, 0, 0, 0);
+    
+    
+    /*
+     * for(int j = 0; j < 195; j++){
+     *  strip.setPixelColor(i, 255, 0, 255);
+     * } 
+     */
     strip.show();
     
     delay(10);
